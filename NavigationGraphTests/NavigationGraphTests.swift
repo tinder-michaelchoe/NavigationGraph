98535920e01f6926d09e013a99b00eee069bbd01
Michael Choe <michael@straussmade.com>
2025-07-11 21:14:23 -0700

    Initial Commit

---
+++ NavigationGraphTests/NavigationGraphTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationGraphTests.swift
+//  NavigationGraphTests
+//
+//  Created by mexicanpizza on 7/11/25.
+//
+
+import Testing
+@testable import NavigationGraph
+
+struct NavigationGraphTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
